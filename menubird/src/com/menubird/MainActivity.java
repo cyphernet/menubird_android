84a67f6f39460ad1b2a193c0d4595ef56a1bb15b
Ziqi Jin <jinziqi@gmail.com>
2013-04-26 09:54:41 -0400

    request to server

--- menubird/src/com/menubird/MainActivity.java
+++ menubird/src/com/menubird/MainActivity.java
@@ -1,15 +1,56 @@
 package com.menubird;
 
+import java.io.*;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.entity.mime.*;
+import org.apache.http.entity.mime.content.*;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.hardware.Camera;
+import android.hardware.Camera.*;
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
 
 public class MainActivity extends Activity {
 
+	protected static final String TAG = "MainActivity";
+	private Camera mCamera;
+    private CameraPreview mPreview;
+    
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		// Create an instance of Camera
+        mCamera = getCameraInstance();
+
+        // Create our Preview view and set it as the content of our activity.
+        mPreview = new CameraPreview(this, mCamera);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
+        
+        // Add a listener to the Capture button
+        Button captureButton = (Button) findViewById(R.id.button_capture);
+        captureButton.setOnClickListener(
+            new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // get an image from the camera
+                    mCamera.takePicture(null, null, mPicture);
+                }
+            }
+        );
 	}
 
 	@Override
@@ -18,5 +59,51 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	public static Camera getCameraInstance(){
+	    Camera c = null;
+	    try {
+	        c = Camera.open(); // attempt to get a Camera instance
+	    }
+	    catch (Exception e){
+	        // Camera is not available (in use or does not exist)
+	    }
+	    return c; // returns null if camera is unavailable
+	}
+	
+	private PictureCallback mPicture = new PictureCallback() {
+
+	    @Override
+	    public void onPictureTaken(byte[] data, Camera camera) {
+
+	    	Bitmap srcBmp=BitmapFactory.decodeByteArray(data,0,data.length);
+	    	Bitmap dstBmp = Bitmap.createBitmap(
+	    		     srcBmp, 
+	    		     srcBmp.getWidth()/2 - srcBmp.getHeight()/2,
+	    		     0,
+	    		     srcBmp.getHeight(), 
+	    		     srcBmp.getHeight()
+	    		     );
+	    	
+	    	dstBmp.compress(Bitmap.CompressFormat.JPEG, 70, outStream);
+	    	
+	    	HttpClient httpClient = new DefaultHttpClient();
+	    	HttpPost httpPost = new HttpPost("http://www.yoursite.com/script.php");
+	    	
+	    	try {
+	            MultipartEntity entity = new MultipartEntity(HttpMultipartMode.BROWSER_COMPATIBLE);
+	            entity.addPart("image", new ByteArrayBody(data, "image.jpg"));
+	            entity.addPart("key", new StringBody("value"));
+
+	            httpPost.setEntity(entity);
+
+	            HttpResponse response = httpClient.execute(httpPost, localContext);
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	    	
+	    	mCamera.startPreview();
+	    }
+	};
 
 }
